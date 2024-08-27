2163ef812d03812abea3dd4bd0d1621322ae213f
AnnulusGames <annulusgames@gmail.com>
2024-08-27 21:43:24 +0900

    Add: sync methods to IResourceProvider

--- src/NavStack/Assets/NavStack/Runtime/Content/AddressablesResourceProvider.cs
+++ src/NavStack/Assets/NavStack/Runtime/Content/AddressablesResourceProvider.cs
@@ -7,6 +7,17 @@ namespace NavStack.Content
 {
     internal sealed class AddressablesResourceProvider : IResourceProvider
     {
+        public T Load<T>(string key) where T : UnityEngine.Object
+        {
+            var result = Addressables.LoadAssetAsync<T>(key).WaitForCompletion();
+            return result;
+        }
+
+        public void Unload<T>(T obj) where T : UnityEngine.Object
+        {
+            Addressables.Release(obj);
+        }
+
         public async UniTask<T> LoadAsync<T>(string key, CancellationToken cancellationToken = default)
             where T : UnityEngine.Object
         {
