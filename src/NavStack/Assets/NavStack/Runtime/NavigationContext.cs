19ed94e77a98bdcccf20277adddba81dd70dfab0
AnnulusGames <annulusgames@gmail.com>
2024-08-28 00:01:30 +0900

    Change: default AwaitOperation

--- src/NavStack/Assets/NavStack/Runtime/NavigationContext.cs
+++ src/NavStack/Assets/NavStack/Runtime/NavigationContext.cs
@@ -2,7 +2,7 @@ namespace NavStack
 {
     public record NavigationContext
     {
-        public NavigationAwaitOperation AwaitOperation { get; init; } = NavigationAwaitOperation.Error;
+        public NavigationAwaitOperation AwaitOperation { get; init; } = NavigationAwaitOperation.Sequential;
         public NavigationParameters Parameters { get; init; } = new();
     }
 }
